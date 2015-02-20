99dc974219ba1ad781c6b5e54ba4842af2e50835
Axel Davy <axel.davy@ens.fr>
2015-02-20 12:11:56 +0100

    st/nine: Some D3DUSAGE_AUTOGENMIPMAP fixes
    Signed-off-by: Axel Davy <axel.davy@ens.fr>

--- src/gallium/state_trackers/nine/surface9.c
+++ src/gallium/state_trackers/nine/surface9.c
@@ -231,7 +231,7 @@ NineSurface9_GetContainer( struct NineSurface9 *This,
     return hr;
 }
 
-static INLINE void
+void
 NineSurface9_MarkContainerDirty( struct NineSurface9 *This )
 {
     if (This->texture) {
